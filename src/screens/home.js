a7c45dc35804e9143bb3e1f55a53ef71b6b1ecfb
muhammadasadullahtariq <asadullahtariq8995@gmail.com>
2022-07-14 13:55:06 +0500

    banner added to event id

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Platform,
+} from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {RNCamera} from 'react-native-camera';
 import checkEvent from '../api/checkEvent';
@@ -13,6 +20,9 @@ const Screen = () => {
   const navigation = useNavigation(); // eslint-disable-line
   const [flash, setFlash] = useState(false);
   const [waitingAlertFlag, setWaitingAlertFlag] = useState(false);
+  const [showBanner, setShowBanner] = useState(false);
+  const [bannerText, setBannerText] = useState('');
+  const [bannerColor, setBannerColor] = useState('');
 
   useEffect(() => {
     (async function checkEvent() {
@@ -41,11 +51,15 @@ const Screen = () => {
     if (data.status === 1) {
       navigation.reset({routes: [{name: 'ScanTicket'}]});
     } else if (data.status === 2) {
-      navigation.navigate('ScanTicket');
+      setShowBanner(true);
+      setBannerText(data.text);
+      setBannerColor(data.color);
     } else if (data.status === 3) {
       showToast('Not valid QR code');
+      setShowBanner(false);
     } else if (data.status === 'erro') {
       showToast('Some error occure please try again');
+      setShowBanner(false);
     }
   };
 
@@ -83,6 +97,29 @@ const Screen = () => {
             </View>
           </View>
         }
+        bottomContent={
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{
+              alignItems: 'center',
+              width: '100%',
+            }}
+            style={{
+              width: '100%',
+              paddingHorizontal: 10,
+              marginTop: Platform.OS != 'ios' ? 50 : 0,
+            }}>
+            {showBanner ? (
+              <View
+                style={[
+                  styles.bannerContainer,
+                  {backgroundColor: bannerColor},
+                ]}>
+                <Text style={styles.bannerText}>{bannerText}</Text>
+              </View>
+            ) : null}
+          </ScrollView>
+        }
       />
     </View>
   );
@@ -115,6 +152,18 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     width: '90%',
     marginBottom: 20,
+  },
+  bannerContainer: {
+    marginTop: 20,
+    width: '100%',
+    minHeight: 100,
+    backgroundColor: 'red',
+  },
+  bannerText: {
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    fontSize: 18,
+    fontWeight: '500',
   },
 });
 
